f784421b2560ae4391b00ab4271e7d85e6fc1d33
Kilian <kilian@mirdan.de>
2015-10-01 19:37:39 +0200

    Remove old survey data from phpBB3.0.x after conversion

---
+++ migrations/delete_old_data_from_phpbb30.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ * @package survey
+ * @copyright (c) 2015 kilianr
+ * @license http://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License v3
+ *
+ */
+
+namespace kilianr\survey\migrations;
+
+use kilianr\survey\functions\survey;
+
+class delete_old_data_from_phpbb30 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\kilianr\survey\migrations\convert_from_phpbb30');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'drop_tables' => array(
+				$this->table_prefix . 'survey',
+				$this->table_prefix . 'survey_answers',
+			),
+			'drop_columns' => array(
+				TOPICS_TABLE => array('topic_survey',),
+			),
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('permission.remove', array('f_survey_design', false)),
+			array('permission.remove', array('f_survey_takeforothers', false)),
+			array('permission.remove', array('f_survey_viewhiddenresults', false)),
+			array('config.remove', array('survey_version')),
+		);
+	}
+}
